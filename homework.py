0a6e3a3022eedbb6e7fdc43cc3e7a4aad0efb820
Jennifer0617 <68540663+Jennifer0617@users.noreply.github.com>
2020-07-27 18:04:06 +0800

    7/27hw
    90分有禮物

---
+++ homework.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 18:01:10 2020
+
+@author: 林玉鑫
+"""
+
+
+a=input("輸入數學成績拉齁")
+ｂ=input("輸入英文成績拉齁")
+ｃ=int(a,b)
+if c >= 90:
+    print("有獎品喔齁")
+else:
+    print("處罰罰起來")
