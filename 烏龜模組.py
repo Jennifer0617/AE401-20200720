f6216e30414f4e1838a43e99f2a3ec962a1ef205
Jennifer0617 <68540663+Jennifer0617@users.noreply.github.com>
2020-08-10 18:01:20 +0800

    烏龜背景~~20200810

--- 烏龜模組.py
+++ 烏龜模組.py
@@ -1,8 +1,11 @@
 import turtle
 import time
+b=turtle.Screen()
+b.title("my window")
+b.bgcolor("black")
 
 t = turtle.Turtle()
-t.color("black")
+t.color("white")
 t.shape("turtle")
 t.forward(100)
 t.left(90)
