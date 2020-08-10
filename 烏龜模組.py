07b6a677d6b85d6405559ec5483c180279615abd
Jennifer0617 <68540663+Jennifer0617@users.noreply.github.com>
2020-08-10 17:27:25 +0800

    烏龜模組20200810

---
+++ 烏龜模組.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+t.color("black")
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
