f11ee2f9a46ba63a07468283fa96422ab1f2547d
Jennifer0617 <68540663+Jennifer0617@users.noreply.github.com>
2020-08-10 15:52:36 +0800

    20200810
    ramdon

--- random.py
+++ random.py
@@ -6,9 +6,17 @@ Created on Mon Aug  3 17:30:38 2020
 """
 import random
 ans = random.randint(1,10)
-n=input("猜一個數字")
-n=int(n)
-if ans == n:
-   print("你很棒")
-else:
-   print("你加油 答案是",ans)
+i = 0
+
+while i<5:
+    n=input("猜一個數字")
+    n=int(n)
+    if ans < n :
+       print("小一點")
+    elif ans>n:
+        print("大一點")
+    else:
+        print("猜對了")
+        break
+    i=i+1
+       
